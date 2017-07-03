69013b3375bef30a31705b0bf24158f5c43b2631
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2017-07-03 18:21:36 +0300

    #1 Create order of activity planning variable exploration depending on number of excluded timeslots.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/domain/solver/TimeslotWeightFactory.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/domain/solver/TimeslotWeightFactory.java
@@ -1,33 +1,27 @@
 package org.aimas.cami.scheduler.CAMIScheduler.domain.solver;
 
+import org.aimas.cami.scheduler.CAMIScheduler.domain.ActivityPeriod;
 import org.aimas.cami.scheduler.CAMIScheduler.domain.ActivitySchedule;
 import org.aimas.cami.scheduler.CAMIScheduler.domain.ExcludedTimeslotsPenalty;
 import org.aimas.cami.scheduler.CAMIScheduler.domain.Timeslot;
 import org.apache.commons.lang3.builder.CompareToBuilder;
 import org.optaplanner.core.impl.heuristic.selector.common.decorator.SelectionSorterWeightFactory;
 
-public class TimeslotWeightFactory implements SelectionSorterWeightFactory<ActivitySchedule, Timeslot> {
-
-	@Override
-	public Comparable createSorterWeight(ActivitySchedule solution, Timeslot selection) {
-		int penalty = 0;
-		for (ExcludedTimeslotsPenalty etp : solution.getExcludedTimeslotsList()) {
-			Timeslot excluded = etp.getExcludedTimeslot();
-			if (selection.getTimeslot()[0] >= excluded.getTimeslot()[0]
-					&& selection.getTimeslot()[1] <= excluded.getTimeslot()[1]) {
-				penalty++;
-			}
-		}
-		return new TimeslotWeight(selection, penalty);
-	}
+/**
+ * 
+ * @author Bogdan
+ *
+ */
+public class TimeslotWeightFactory implements SelectionSorterWeightFactory<ActivitySchedule, ActivityPeriod> {
 
 	public static class TimeslotWeight implements Comparable<TimeslotWeight> {
 
-		private final Timeslot timeslot;
+		private final ActivityPeriod period;
 		private final int penalty;
 
-		public TimeslotWeight(Timeslot timeslot, int penalty) {
-			this.timeslot = timeslot;
+		public TimeslotWeight(ActivityPeriod period, int penalty) {
+			super();
+			this.period = period;
 			this.penalty = penalty;
 		}
 
@@ -35,10 +29,35 @@ public class TimeslotWeightFactory implements SelectionSorterWeightFactory<Activ
 		public int compareTo(TimeslotWeight weight) {
 			// bigger penalty -> weaker
 			return new CompareToBuilder().append(weight.penalty, this.penalty)
-					.append(timeslot.getTimeslot(), weight.timeslot.getTimeslot())
-					.append(timeslot.getId(), weight.timeslot.getId()).toComparison();
+					.append(period.getWeekDay().getDayIndex(), weight.period.getWeekDay().getDayIndex())
+					.append(period.getTimeslot().getTimeslot()[0].getHour(),
+							weight.period.getTimeslot().getTimeslot()[0].getHour())
+					.append(period.getTimeslot().getTimeslot()[0].getMinutes(),
+							weight.period.getTimeslot().getTimeslot()[0].getMinutes())
+					.append(period.getId(), weight.period.getId()).toComparison();
 		}
 
+	}
+
+	@Override
+	public Comparable createSorterWeight(ActivitySchedule solution, ActivityPeriod selection) {
+		int penalty = 0;
+		for (ExcludedTimeslotsPenalty etp : solution.getExcludedTimeslotsList()) {
+			for (ActivityPeriod excludedPeriod : etp.getExcludedActivityPeriods()) {
+				Timeslot excludedTimeslot = excludedPeriod.getTimeslot();
+				if (selection.getTimeslot().getTimeslot()[0].getHour() >= excludedTimeslot.getTimeslot()[0].getHour()
+						&& selection.getTimeslot().getTimeslot()[0].getMinutes() >= excludedTimeslot.getTimeslot()[0]
+								.getMinutes()
+						&& selection.getTimeslot().getTimeslot()[1].getHour() <= excludedTimeslot.getTimeslot()[1]
+								.getHour()
+						&& selection.getTimeslot().getTimeslot()[1].getMinutes() <= excludedTimeslot.getTimeslot()[1]
+								.getMinutes()
+						&& selection.getWeekDay() == excludedPeriod.getWeekDay()) {
+					penalty++;
+				}
+			}
+		}
+		return new TimeslotWeight(selection, penalty);
 	}
 
 }
