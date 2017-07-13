a015311dab7c3896ea635873b51bcc22b51d2ce2
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2017-07-13 18:59:55 +0300

    #1 Auxiliary methods for activity possible interval exploration during and filter for imposed activities

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/solver/move/ActivityPeriodMoveFactory.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/solver/move/ActivityPeriodMoveFactory.java
@@ -1,13 +1,15 @@
 package org.aimas.cami.scheduler.CAMIScheduler.solver.move;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.aimas.cami.scheduler.CAMIScheduler.domain.Activity;
 import org.aimas.cami.scheduler.CAMIScheduler.domain.ActivitySchedule;
-import org.optaplanner.core.impl.heuristic.move.Move;
+import org.aimas.cami.scheduler.CAMIScheduler.domain.TimeInterval;
 import org.optaplanner.core.impl.heuristic.selector.move.factory.MoveListFactory;
 
 /**
- * for now, not sure if needed
+ * Generate moves for ActivityPeriodMove class.
  * 
  * @author Bogdan
  *
@@ -15,9 +17,26 @@ import org.optaplanner.core.impl.heuristic.selector.move.factory.MoveListFactory
 public class ActivityPeriodMoveFactory implements MoveListFactory<ActivitySchedule> {
 
 	@Override
-	public List<? extends Move<ActivitySchedule>> createMoveList(ActivitySchedule solution) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<ActivityPeriodMove> createMoveList(ActivitySchedule activitySchedule) {
+		List<ActivityPeriodMove> moveList = new ArrayList<>();
+		List<Activity> activityList = activitySchedule.getActivityList();
+
+		for (Activity activity : activityList) {
+			if (activity.getPermittedInterval() != null) {
+				int minutesInPermittedInterval = getNumberOfMinutesInPermittedInterval(activity.getPermittedInterval())
+						- activity.getActivityDuration();
+				for (int i = 1; i <= minutesInPermittedInterval; i++) {
+					moveList.add(new ActivityPeriodMove(activity, i));
+				}
+			}
+		}
+		return moveList;
+	}
+
+	private Integer getNumberOfMinutesInPermittedInterval(TimeInterval permittedInterval) {
+
+		return (permittedInterval.getMaxEnd().getHour() - permittedInterval.getMinStart().getHour()) * 60
+				+ permittedInterval.getMaxEnd().getMinutes() - permittedInterval.getMinStart().getMinutes();
 	}
 
 }
