2530e4ba9a2116c118a2d09d9d1f6765a3d60022
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2017-08-01 18:47:54 +0300

    #1 Utility classes for drools scores.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/StaticPenaltyScoresDrools.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/StaticPenaltyScoresDrools.java
@@ -2,13 +2,12 @@ package org.aimas.cami.scheduler.CAMIScheduler.utils;
 
 public class StaticPenaltyScoresDrools {
 
-	public static final int periodConflictPenalty = -999;
-	public static final int imposedTimePenalty = -100;
-	public static final int excludedTimeslotsSameDayPenalty = -10;
-	public static final int excludedTimeslotsBetweenDaysPenalty = -25;
-	public static final int relativeActivityPenaltyDifferentDays = -35;
-	public static final int instancesPerWeekPenalty = 15;
-	// for now
-	public static final int instancesPerWeekPatternPenalty = -10;
-	public static final int instancesPerDayPenalty = -5;
+	public static final int periodConflictPenalty = -1;
+	public static final int imposedTimePenalty = -2;
+	public static final int excludedTimeslotsSameDayPenalty = -3;
+	public static final int excludedTimeslotsBetweenDaysPenalty = -4;
+	public static final int relativeActivityBeforePenaltyDifferentDays = -5;
+	public static final int relativeActivityAfterPenaltyDifferentDays = -6;
+	public static final int instancesPerWeekPenalty = -7;
+	public static final int instancesPerDayPenalty = -8;
 }
--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/Utility.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/Utility.java
@@ -1,14 +1,20 @@
 package org.aimas.cami.scheduler.CAMIScheduler.utils;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import org.aimas.cami.scheduler.CAMIScheduler.domain.ActivityPeriod;
+import org.aimas.cami.scheduler.CAMIScheduler.domain.PeriodInterval;
+import org.aimas.cami.scheduler.CAMIScheduler.domain.Time;
+import org.aimas.cami.scheduler.CAMIScheduler.domain.TimeInterval;
 import org.drools.core.spi.KnowledgeHelper;
 
+/**
+ * 
+ * @author Bogdan
+ *
+ */
 public class Utility {
-
-	public static List<Integer> accumulateList;
 
 	public static void help(final KnowledgeHelper drools, final String message) {
 		System.out.println(message);
@@ -19,16 +25,71 @@ public class Utility {
 		System.out.println("\nrule triggered: " + drools.getRule().getName());
 	}
 
-	public static void initializeList() {
-		accumulateList = new ArrayList<>();
+	public static Integer getNumberOfMinutesInPermittedInterval(TimeInterval permittedInterval) {
+
+		return (permittedInterval.getMaxEnd().getHour() - permittedInterval.getMinStart().getHour()) * 60
+				+ permittedInterval.getMaxEnd().getMinutes() - permittedInterval.getMinStart().getMinutes();
 	}
 
-	public static void clearList() {
-		accumulateList.clear();
+	public static Set<Character> stringToCharacterSet(String s) {
+		Set<Character> set = new LinkedHashSet<>();
+		for (char c : s.toCharArray()) {
+			set.add(c);
+		}
+		return set;
 	}
 
-	public static void sortList() {
-		Collections.sort(accumulateList);
+	public static Boolean checkTimeslots(ActivityPeriod activityPeriod, PeriodInterval excludedPeriodInterval,
+			int activityDuration, boolean sameStartDay, boolean sameEndDay) {
+
+		ActivityPeriod activityEndPeriod = AdjustActivityPeriod.getAdjustedPeriod(activityPeriod, activityDuration);
+		Time activityStartTime = activityPeriod.getTime();
+		Time activityEndTime = activityEndPeriod.getTime();
+		Time excludedStartTime = excludedPeriodInterval.getStartPeriod().getTime();
+		Time excludedEndTime = excludedPeriodInterval.getEndPeriod().getTime();
+
+		if (((activityStartTime.getHour() > excludedStartTime.getHour()
+				|| activityEndTime.getHour() > excludedStartTime.getHour()) && sameStartDay)
+				|| (activityStartTime.getHour() < excludedEndTime.getHour() && sameEndDay)) {
+
+			return true;
+
+		} else if (sameStartDay) {
+
+			if (activityStartTime.getHour() == excludedStartTime.getHour()) {
+
+				if ((activityStartTime.getMinutes() + activityDuration) >= excludedStartTime.getMinutes()) {
+					return true;
+				}
+
+			} else if (activityEndTime.getHour() == excludedStartTime.getHour()) {
+
+				if (activityEndTime.getMinutes() > excludedStartTime.getMinutes()) {
+					return true;
+				}
+
+			}
+
+		} else if (sameEndDay) {
+
+			if (activityStartTime.getHour() == excludedEndTime.getHour()) {
+
+				if (activityStartTime.getMinutes() < excludedEndTime.getMinutes()) {
+					return true;
+				}
+
+			}
+
+		} else {
+			if (activityStartTime.getHour() >= excludedStartTime.getHour()
+					&& activityStartTime.getHour() < excludedEndTime.getHour()) {
+
+				return true;
+
+			}
+		}
+
+		return false;
 	}
 
 }
