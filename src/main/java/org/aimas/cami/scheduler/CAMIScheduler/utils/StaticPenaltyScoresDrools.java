d02dfebf89763a07ab65696935a66af1b5c6394d
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2017-08-18 19:21:22 +0300

    #1 Utility classes for drools scores, persistence and benchmark.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/StaticPenaltyScoresDrools.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/StaticPenaltyScoresDrools.java
@@ -2,12 +2,11 @@ package org.aimas.cami.scheduler.CAMIScheduler.utils;
 
 public class StaticPenaltyScoresDrools {
 
-	public static final int periodConflictPenalty = -1;
-	public static final int imposedTimePenalty = -2;
-	public static final int excludedTimeslotsSameDayPenalty = -3;
-	public static final int excludedTimeslotsBetweenDaysPenalty = -4;
-	public static final int relativeActivityBeforePenaltyDifferentDays = -5;
-	public static final int relativeActivityAfterPenaltyDifferentDays = -6;
-	public static final int instancesPerWeekPenalty = -7;
-	public static final int instancesPerDayPenalty = -8;
+	public static final int periodConflictPenalty = -5;
+	public static final int excludedTimeslotsSameDayPenalty = -4;
+	public static final int excludedTimeslotsBetweenDaysPenalty = -3;
+	//public static final int relativeActivityBeforePenaltyDifferentDays = -4;
+	//public static final int relativeActivityAfterPenaltyDifferentDays = -5;
+	public static final int instancesPerWeekPenalty = -2;
+	public static final int instancesPerDayPenalty = -1;
 }
