c30e32d4ee682c70650eb7bd5c897db28eb2b194
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2017-08-25 19:11:43 +0300

    Static scores for drools.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/StaticPenaltyScoresDrools.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/StaticPenaltyScoresDrools.java
@@ -5,8 +5,8 @@ public class StaticPenaltyScoresDrools {
 	public static final int periodConflictPenalty = -5;
 	public static final int excludedTimeslotsSameDayPenalty = -4;
 	public static final int excludedTimeslotsBetweenDaysPenalty = -3;
-	//public static final int relativeActivityBeforePenaltyDifferentDays = -4;
-	//public static final int relativeActivityAfterPenaltyDifferentDays = -5;
+	public static final int relativeActivityBeforePenaltyDifferentDays = -6;
+	public static final int relativeActivityAfterPenaltyDifferentDays = -7;
 	public static final int instancesPerWeekPenalty = -2;
 	public static final int instancesPerDayPenalty = -1;
 }
