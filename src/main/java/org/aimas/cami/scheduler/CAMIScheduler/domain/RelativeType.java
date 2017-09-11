ffb431ea0f4955fc9f9c97b416284a0028eeb8df
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2017-09-11 15:44:31 +0300

    Revert "7 finalize add activity function"

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/domain/RelativeType.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/domain/RelativeType.java
@@ -1,17 +1,10 @@
 package org.aimas.cami.scheduler.CAMIScheduler.domain;
 
-import org.aimas.cami.scheduler.CAMIScheduler.swingui.Labeled;
-
 /**
  * 
  * @author Bogdan
  *
  */
-public enum RelativeType implements Labeled {
-	BEFORE, AFTER;
-
-	@Override
-	public String getLabel() {
-		return this.toString();
-	}
+public enum RelativeType {
+	BEFORE, AFTER
 }
