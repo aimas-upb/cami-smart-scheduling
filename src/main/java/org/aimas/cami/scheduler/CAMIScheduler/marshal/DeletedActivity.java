296d5007a65f727ddf8fa9f1cf109f404428a50e
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2018-06-08 17:59:20 +0300

    Add class for activity deletion marshaling.

---
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/marshal/DeletedActivity.java
@@ -0,0 +1,34 @@
+package org.aimas.cami.scheduler.CAMIScheduler.marshal;
+
+import org.aimas.cami.scheduler.CAMIScheduler.utils.AbstractPersistable;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Represents an activity from the schedule that has to be deleted.
+ * 
+ * @author Bogdan
+ *
+ */
+@XStreamAlias("DeletedActivity")
+public class DeletedActivity extends AbstractPersistable {
+	private String name;
+	private String uuid;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+}
