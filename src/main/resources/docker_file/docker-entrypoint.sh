1dc0d47813ac2ad9f7f000b4a2be11c844e840e0
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2018-07-20 12:19:25 +0300

    Update Dockerfile. Add shell script for entrypoint.

---
+++ src/main/resources/docker_file/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# change to project directory to use the classpath
+cd /cami-smart-scheduling
+
+runtimeOptions="-Xms256m -Xmx1536m -Dorg.optaplanner.examples.dataDir=data/"
+myClasspath="target/dependency/*:target/classes"
+mainClass=org.aimas.cami.scheduler.CAMIScheduler.server.Server
+
+java ${runtimeOptions} -classpath ${myClasspath} ${mainClass} localhost 8080
