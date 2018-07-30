f6c1c565f9a1d9a0bb1672f7ffa01af88dbaa2ef
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2018-07-30 16:12:14 +0300

    #14 Set activity uuid when it's added.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/SolutionUtils.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/utils/SolutionUtils.java
@@ -81,6 +81,7 @@ public class SolutionUtils<Solution_> {
 						activity.setPeriodDomainRangeList(Utility.determineValueRange(activitySchedule, activity));
 						activity.setId(
 								activityList.isEmpty() ? 0L : activityList.get(activityList.size() - 1).getId() + 1);
+						activity.setUuid(Utility.generateRandomUuid());
 
 						scoreDirector.beforeEntityAdded(activity);
 						activityList.add(activity);
@@ -152,6 +153,7 @@ public class SolutionUtils<Solution_> {
 								relativeActivity.setImmovable(newActivity.isImmovable());
 								relativeActivity.setId(activityList.isEmpty() ? 0L
 										: activityList.get(activityList.size() - 1).getId() + 1);
+								relativeActivity.setUuid(Utility.generateRandomUuid());
 
 								scoreDirector.beforeEntityAdded(relativeActivity);
 								activityList.add(relativeActivity);
