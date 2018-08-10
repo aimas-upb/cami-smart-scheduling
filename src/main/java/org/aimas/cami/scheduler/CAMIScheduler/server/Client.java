5dfddac28dbe2ea7236e64b9fd342b5583c25b26
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2018-08-10 15:55:32 +0300

    #17 Add delete request method.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/server/Client.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/server/Client.java
@@ -29,6 +29,7 @@ public class Client extends AbstractVerticle {
 
 		// send requests to the server
 		postNewActivity(client);
+		// deleteActivity(client);
 		getActivitySchedule(client);
 
 		// send multiple requests to analyze the concurrency
@@ -80,5 +81,32 @@ public class Client extends AbstractVerticle {
 				}
 			});
 		}).end();
+	}
+
+	private void deleteActivity(HttpClient client) {
+		String deletedActivityFilePath = "data" + File.separator + "activityschedule" + File.separator
+				+ "Deleted Activities.json";
+
+		String deletedActivity = null;
+
+		try {
+			deletedActivity = new String(Files.readAllBytes(Paths.get(deletedActivityFilePath)));
+			// System.out.println(newActivity);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		client.delete(SERVER_PORT, SERVER_HOST, RoutePaths.API_ROUTE + RoutePaths.DELETE_ACTIVITY_ROUTE, response -> {
+
+			System.out.println("Received response with status code " + response.statusCode());
+
+			response.bodyHandler(new Handler<Buffer>() {
+
+				@Override
+				public void handle(Buffer event) {
+					System.out.println("Got the event: " + event);
+				}
+			});
+		}).end(deletedActivity);
 	}
 }
