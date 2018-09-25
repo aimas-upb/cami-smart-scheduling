221d0eaa28f2e2ef1a9c2040a110c70d0e3724f1
JohnBogdan1 <bogdan_ionut.ion@cti.pub.ro>
2018-09-25 22:35:13 +0300

    #34 Tested only relative activities.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/persistence/CAMITaskSchedulerGenerator.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/persistence/CAMITaskSchedulerGenerator.java
@@ -296,7 +296,7 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 			activityList.add(activity);
 		}
 
-		{
+		/*{
 			ActivityType stretches = new ActivityType();
 			stretches.setActivityCategory(activitySchedule.getActivityCategoryList().get(0));
 			stretches.setCode("Stretches");
@@ -496,7 +496,7 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 			activity.setImmovable(true);
 			activity.setOnDropdown(true);
 			activityList.add(activity);
-		}
+		}*/
 
 		// ---------------------------------------------------//
 
@@ -535,7 +535,7 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 
 		// ---------------------------------------------------//
 
-		{
+		/*{
 			ActivityType bloodPressurehMeasurement = new ActivityType();
 			bloodPressurehMeasurement.setActivityCategory(activitySchedule.getActivityCategoryList().get(2));
 			bloodPressurehMeasurement.setCode("Blood pressure measurement");
@@ -553,7 +553,7 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 				activity.setOnDropdown(true);
 				activityList.add(activity);
 			}
-		}
+		}*/
 
 		// ---------------------------------------------------//
 
@@ -623,7 +623,7 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 
 			relativeActivityPenaltyList.add(relativeActivityPenalty);
 
-			ExcludedTimePeriodsPenalty etpp = new ExcludedTimePeriodsPenalty();
+			/*ExcludedTimePeriodsPenalty etpp = new ExcludedTimePeriodsPenalty();
 			etpp.setActivityType(antibiotic);
 
 			etpp.setId(exccludedId++);
@@ -641,11 +641,11 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 			excludedActivityPeriods.add(pi2);
 			etpp.setExcludedActivityPeriods(excludedActivityPeriods);
 
-			excludedTimePeriodsPenaltyList.add(etpp);
+			excludedTimePeriodsPenaltyList.add(etpp);*/
 		}
 
 		// ---------------------------------------------------//
-		{
+		/*{
 			ActivityType tvSeries = new ActivityType();
 			tvSeries.setActivityCategory(activitySchedule.getActivityCategoryList().get(4));
 			tvSeries.setCode("Favorite TV Series 1");
@@ -707,7 +707,7 @@ public class CAMITaskSchedulerGenerator extends LoggingMain {
 			// activity.setWantedToBePlanned(true);
 			activity.setOnDropdown(true);
 			activityList.add(activity);
-		}
+		}*/
 
 		activitySchedule.setActivityTypeList(activityTypeList);
 		activitySchedule.setActivityList(activityList);
