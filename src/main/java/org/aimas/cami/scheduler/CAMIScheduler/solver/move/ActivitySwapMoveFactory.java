5c0c112f1f2fee56796eff994f190d90d046484b
JohnPrime <bogdan_ionut.ion@cti.pub.ro>
2018-10-04 16:45:58 +0300

    #34 Create custom swap moves only between relatively constrained activities.

--- src/main/java/org/aimas/cami/scheduler/CAMIScheduler/solver/move/ActivitySwapMoveFactory.java
+++ src/main/java/org/aimas/cami/scheduler/CAMIScheduler/solver/move/ActivitySwapMoveFactory.java
@@ -6,6 +6,8 @@ import java.util.ListIterator;
 
 import org.aimas.cami.scheduler.CAMIScheduler.domain.Activity;
 import org.aimas.cami.scheduler.CAMIScheduler.domain.ActivitySchedule;
+import org.aimas.cami.scheduler.CAMIScheduler.domain.NormalActivity;
+import org.aimas.cami.scheduler.CAMIScheduler.domain.RelativeActivityPenalty;
 import org.optaplanner.core.impl.heuristic.move.Move;
 import org.optaplanner.core.impl.heuristic.selector.move.factory.MoveListFactory;
 
@@ -18,12 +20,33 @@ public class ActivitySwapMoveFactory implements MoveListFactory<ActivitySchedule
 
 		for (ListIterator<Activity> leftIt = activityList.listIterator(); leftIt.hasNext();) {
 			Activity leftActivity = leftIt.next();
+			if (leftActivity.isImmovable() || !haveRelativeConstraints(leftActivity, solution))
+				continue;
 			for (ListIterator<Activity> rightIt = activityList.listIterator(leftIt.nextIndex()); rightIt.hasNext();) {
 				Activity rightActivity = rightIt.next();
+				if (rightActivity.isImmovable() || !haveRelativeConstraints(rightActivity, solution))
+					continue;
 				moveList.add(new ActivitySwapMove(leftActivity, rightActivity));
 			}
 		}
 		return moveList;
+	}
+
+	private boolean haveRelativeConstraints(Activity activity, ActivitySchedule solution) {
+
+		for (RelativeActivityPenalty rap : solution.getRelativeActivityPenaltyList()) {
+			// if it is part of a relativity constraint
+
+			if (activity instanceof NormalActivity) {
+				if (rap.getCategory() != null && activity.getActivityCategory().getCode().equals(rap.getCategory()))
+					return true;
+			} else {
+				if (rap.getCategory() != null && rap.getRelativeActivityType().equals(activity.getActivityTypeCode()))
+					return true;
+			}
+
+		}
+		return false;
 	}
 
 }
